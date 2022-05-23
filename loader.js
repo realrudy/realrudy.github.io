a0a53965f9d169fe621c3e72d05910272be7dfe5
Your Name <105519606+bluefish1678@users.noreply.github.com>
2022-05-23 11:38:21 -0400

    test

--- loader.js
+++ loader.js
@@ -6,3 +6,8 @@ setTimeout(function () {
   blog.style.display = "block";
   loader.style.display = "none";
 }, 1000);
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  // some code..
+ document.body.innerHTML = 'test';
+ }
